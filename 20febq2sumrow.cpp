27fb7e9bd7c571ad3bd31617578092a6beba872c
Adarsh Raj <83593533+Rajadarsh55@users.noreply.github.com>
2023-02-20 15:18:46 +0530

    Add files via upload

---
+++ 20febq2sumrow.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int matrix[3][3] = {
+	{1,2,3},
+	{3,1,2},
+	{2,3,1}
+	};
+	int row_sum = 0;
+	int middle_row=sizeof(matrix)/sizeof(matrix[0])/2;
+	for (int i = 0; i<sizeof(matrix[0])/sizeof(int); i++) {
+row_sum += matrix[middle_row][i];		
+	}
+	
+	
+	int col_sum = 0;
+	int middle_col=sizeof(matrix[0])/sizeof(int)/2;
+	for (int i = 0; i<sizeof(matrix)/sizeof(matrix[0]); i++) {
+col_sum += matrix[i][middle_col];		
+	}
+    cout << "Sum of middle row: " << row_sum << endl;
+    cout << "Sum of middle column: " << col_sum << endl;
+
+    return 0;
+}
+
